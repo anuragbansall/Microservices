2dd59ef841f2c62c953e607c6312a5bdeefd1311
Anurag Bansal <devanuragbansal@gmail.com>
2025-10-15 05:39:14 +0530

    Enhance token retrieval in verifyToken middleware to support authorization headers

--- user/middleware/user.middleware.js
+++ user/middleware/user.middleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import BlacklistToken from "../models/blacklistToken.model.js";
 
 export const verifyToken = async (req, res, next) => {
-  const token = req.cookies.token;
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ error: "Unauthorized" });
