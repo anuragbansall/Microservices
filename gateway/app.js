7a09b6797be2212df66805636e1be8f9f8e87878
Anurag Bansal <devanuragbansal@gmail.com>
2025-10-15 08:58:44 +0530

    Initialize gateway project with package.json and essential dependencies

--- gateway/app.js
+++ gateway/app.js
@@ -1,14 +1,12 @@
 import express from "express";
-import cookieParser from "cookie-parser";
-import userRouter from "./routes/user.routes.js";
+import proxy from "express-http-proxy";
 import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cookieParser());
 
-app.use("/", userRouter);
+app.use("/users", proxy("http://localhost:3001"));
 
 export default app;
